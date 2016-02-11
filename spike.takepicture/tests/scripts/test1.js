0d9ea908dd1c3d22ffa75c799f38a3deaa915729
Rosana Ruiz <rosanarufer@gmail.com>
2016-02-11 13:31:25 +0100

    Minor details

--- spike.takepicture/tests/scripts/test1.js
+++ spike.takepicture/tests/scripts/test1.js
@@ -1,7 +1,6 @@
 $( "#button-send-image" ).click(function() {
-  alert( "Handler for .click() called." );
-
-
+  alert( "Upload file not implemented, authentication is needed" );
+  //uploadFile();
 });
 
 function readURL(input){
@@ -22,3 +21,22 @@ $("#input-add-image").change(function(){
   readURL(this);
 
 });
+
+function uploadFile() {
+    blobFile = $('#input-add-image').files[0]);
+    var fd = new FormData();
+    fd.append("capturedImage", blobFile);
+
+    $.ajax({
+       url: "",
+       type: "POST",
+       data: fd,
+       processData: false,
+       contentType: false,
+       success: function(response) {
+       },
+       error: function(jqXHR, textStatus, errorMessage) {
+           console.log(errorMessage);
+       }
+    });
+}

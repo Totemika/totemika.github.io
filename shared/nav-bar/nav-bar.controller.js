636175d6d0f37d457a95e62a816c9b438bd3b689
Totemika <rosanarufer@gmail.com>
2017-04-17 00:33:34 +0200

    Revert "replaced router by hide/show"
    This reverts commit 87e0d69070a5d600e7a867b97c75d66ad4f50bb4.

--- shared/nav-bar/nav-bar.controller.js
+++
@@ -1,5 +0,0 @@
-totemikaSiteApp.controller("navBarController", ['$scope', function($scope, $rootScope, twitterService) {
-    $scope.select = function(tabName){
-        $scope.selectedTab = tabName;
-    }
-}]);
--- shared/nav-bar/nav-bar.directive.js
+++ shared/nav-bar/nav-bar.directive.js
@@ -1,9 +1,5 @@
 totemikaSiteApp.directive("tsNavBar", function(){
    return {
-       controller: 'navBarController',
-       bindToController: {
-         activeTab: '='
-       },
        replace: true,
        templateUrl: function(elem, attr) { return "shared/nav-bar/nav-bar.template.html"; }
    } 

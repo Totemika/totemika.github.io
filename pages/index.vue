f3f782b9ca554770c787f1274cee514c4046c226
rosanarufer <rosanarufer@gmail.com>
2021-03-08 21:24:22 +0000

    -

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
-<template>
+<template class="container">
    <div>
      <ul>
        <li><a href="/blog/first">Rebuilding this site using Nuxt</a></li>
+       <li><a href="/blog/2">Are obvious things really obvious?</a></li>
      </ul>
   </div>
 </template>
+<style>
+ a {
+   color: black;
+ }
+</style>

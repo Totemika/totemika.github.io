9edea89082c2e116975cc48a9aba4088633c70bf
rosanarufer <rosanarufer@gmail.com>
2021-04-01 21:59:16 +0100

    Generate

--- dist/_nuxt/static/1617310625/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcv","\u002F","\u002Flists","\u002Fcontact","\u002Fblog\u002F1","\u002Fblog\u002F2"]})
--- dist/_nuxt/static/1617310625/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617310625",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9c74027e"}}}}(null,"\u002F"));

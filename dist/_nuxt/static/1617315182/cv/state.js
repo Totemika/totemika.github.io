7b8686e9d63f4a4109bc4ecec12affb1e04c5ff3
rosanarufer <rosanarufer@gmail.com>
2021-04-01 23:13:50 +0100

    generate

--- dist/_nuxt/static/1617315182/cv/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617315182",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcv",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"013ebc4e"}}}}(null));

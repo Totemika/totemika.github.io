3ccf10aecb0c59470c1e201a53c82a3a408ae11c
rosanarufer <rosanarufer@gmail.com>
2021-04-01 23:53:08 +0100

    generate

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
-      <h1>Rosana Rufer</h1>
+    <h1>Rosana Rufer</h1>
+    <div id="navlinks">
       <NuxtLink to="/">Blog</NuxtLink>
       <a href="/old-site/index.html">CV (old site)</a>
       <NuxtLink to="/contact">Contact</NuxtLink>
-       <hr>
-      <Nuxt />
+    </div>
+    <hr>
+    <Nuxt/>
   </div>
 </template>
